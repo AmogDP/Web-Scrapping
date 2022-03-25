4b514d23a4e71c4da65b52048bd14f04ed4026d2
AmogDP <amogh.d.prasanna@gmail.com>
2022-03-25 23:29:41 +0530

    promises

--- activity/cb_promises/cb/parallel.js
+++ activity/cb_promises/cb/parallel.js
@@ -2,3 +2,30 @@
 /* while uploading a video, you want to do some other work
 this is parallel behaviour async
 */
+
+// let myPromise = new Promise(function(resolve,reject){
+//     our code will come here
+
+// })
+
+
+
+let promise = new Promise(function(resolve,reject){
+    const a = 4;
+    const b = 4;
+    if(a == b){
+        resolve("Both are equal");
+    }else{
+        reject("Both are not equal");
+    }
+});
+
+
+promise.then(function(data){
+    console.log(data);
+})
+
+promise.catch(function(error){
+    console.log(error);
+})
+
